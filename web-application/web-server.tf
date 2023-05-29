ddc64f39e2282fcd6e0a485a4f301e52c335cb77
Kyle Flood <kyledavidflood@gmail.com>
2023-05-29 12:03:33 -0400

    web-application: configured ec2 and security group

--- web-application/web-server.tf
+++ web-application/web-server.tf
@@ -1,34 +1,41 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ACCESSKEY
-    secret_key = SECRETKEY
+}
+
+variable "home_ip" {
+    type = string
 }
 
 resource "aws_instance" "web-server" {
-    ami = "ami"
+    ami = "ami-0715c1897453cabd1"
     instance_type = "t2.micro"
-
-    user_data = <<-EOF
-
-    #!/bin/bash
-    sudo su
-    yum update -y
-    yup install httpd -y
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<html>" >> /var/www/html/index.html
-    echo "<head>" >> /var/www/html/index.html
-    echo "<title>Hello World</title>" >> /var/www/html/index.html
-    echo "</head>" >> /var/www/html/index.html
-    echo "<body>" >> /var/www/html/index.html
-    echo "<h1>Hello World!</h1>" >> /var/www/html/index.html
-    echo "</body>" >> /var/www/html/index.html
-    echo "</html>" >> /var/www/html/index.html
-    EOF
+    key_name = "kf_admin"
+    vpc_security_group_ids = ["web-server-sg"]
 
     tags = {
         Name = "web-server"
-        Owner = "DevOps"
+        Owner = "kf"
     }
 
+}
+
+resource "aws_security_group" "web-server-sg" {
+  name        = "web-server-sg"
+  vpc_id      = "vpc-8892f5f5"
+
+  ingress {
+    description      = "allow all traffic from home ip"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = [var.home_ip]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
